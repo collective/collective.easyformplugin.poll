b016ef652b9d3965301d49e7fff099927f96e108
Oshane Bailey <b4.oshany@gmail.com>
2020-06-05 15:54:58 +0000

    Update the permission for anonymous users.

--- src/collective/easyformplugin/poll/browser/viewlet.py
+++ src/collective/easyformplugin/poll/browser/viewlet.py
@@ -3,7 +3,7 @@ from plone.app.layout.viewlets import common as base
 from zope.annotation.interfaces import IAnnotations
 
 from collective.easyformplugin.poll import _
-from collective.easyformplugin.poll import CEFPSS
+from collective.easyformplugin.poll import CEFPSS,  CEFPSV
 
 
 class PollEasyFormViewlet(base.ViewletBase):
@@ -12,11 +12,15 @@ class PollEasyFormViewlet(base.ViewletBase):
     def allowSingleSubmission(self):
         try:
             if not api.user.is_anonymous():
-                if api.user.has_permission('cmf.ModifyPortalContent'):
-                    return False
                 if self.context.Type() == 'EasyForm':
                     annotations = IAnnotations(self.context)
-                    return annotations.get(CEFPSS, False)
+                    singleSubmission = annotations.get(CEFPSS, False)
+                    
+                    voters = annotations.get(CEFPSV, [])
+                    auth_user = api.user.get_current()
+                    if singleSubmission and auth_user.id in voters:
+                        return True
+                    
         except:
             return False
         return False
